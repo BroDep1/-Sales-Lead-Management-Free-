aaebfe4a0de166be96942f89b5d01f2187d0733d
dnovysh <dnovysh@gmail.com>
2024-08-05 21:19:00 +0300

    feat(auth): user authentication

--- src/main/java/com/expohack/slm/authentication/model/dto/CompanyDto.java
+++ src/main/java/com/expohack/slm/authentication/model/dto/CompanyDto.java
@@ -1,7 +1,8 @@
 package com.expohack.slm.authentication.model.dto;
 
+import java.io.Serializable;
 import java.util.UUID;
 
-public record CompanyDto(UUID id, String name) {
+public record CompanyDto(UUID id, String name) implements Serializable {
 
 }
