992b8d5a52940530c5e2bdefe5908494789f9ce1
Maksim Shiryaev <shimady563@gmail.com>
2024-08-05 13:30:55 +0300

    feat(model): create model for matching products

---
+++ src/main/java/com/expohack/slm/model/ProductMatch.java
@@ -0,0 +1,30 @@
+package com.expohack.slm.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class ProductMatch {
+
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  @ManyToOne
+  @JoinColumn(name = "product_id")
+  private Product product;
+
+  @ManyToOne
+  @JoinColumn(name = "company_id")
+  private Company company;
+
+  private String companyProductName;
+}
