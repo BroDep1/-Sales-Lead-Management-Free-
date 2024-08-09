ba9aad94d9e2f71621dff056a6fd46265f5bbdf4
Mukhammadnur <gsjxne@gmail.com>
2024-08-09 21:34:43 +0500

    add increment for sale

--- src/main/java/com/expohack/slm/matching/service/SaleService.java
+++ src/main/java/com/expohack/slm/matching/service/SaleService.java
@@ -5,6 +5,7 @@ import com.expohack.slm.matching.repository.SaleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -12,9 +13,11 @@ import org.springframework.stereotype.Service;
 public class SaleService {
 
   private final SaleRepository saleRepository;
+  private final ProductService productService;
 
   public void save(Sale newSale) {
     saleRepository.save(newSale);
+    productService.increaseTheNumberOfPurchases(newSale);
     log.info("Saved sale with id {}", newSale.getId());
   }
 }
